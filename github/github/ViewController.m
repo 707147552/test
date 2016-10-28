9bcdabdf37ac4ac3867c243b5a23cbbb526b0ac7
707147552 <707147552@qq.com>
2016-10-28 10:57:59 +0800

    hjgj
    fgfhf

--- github/github/ViewController.m
+++ github/github/ViewController.m
@@ -17,8 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor cyanColor];
-    NSLog(@"我草拟妹");
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
